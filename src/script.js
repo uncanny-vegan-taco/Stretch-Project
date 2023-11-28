f0a802209dadbb7581c2c7c9a7d934c7390d8954
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-28 12:25:29 -0800

    Testing Shoulders

--- src/script.js
+++ src/script.js
@@ -5,3 +5,9 @@ neck.addEventListener("click", () => {
   media.innerHTML =
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/JUP_YdYyfQw?si=ysTeSZMCmnjBM0hT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 });
+
+let shoulders = document.querySelector("#shoulders");
+shoulders.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/JUP_YdYyfQw?si=ysTeSZMCmnjBM0hT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
