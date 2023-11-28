a9a459970e5c307c000066dc33d5808b716506b3
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-28 12:24:05 -0800

    Created structure

---
+++ src/script.js
@@ -0,0 +1,7 @@
+let media = document.querySelector("#media");
+
+let neck = document.querySelector("#neck");
+neck.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/JUP_YdYyfQw?si=ysTeSZMCmnjBM0hT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
