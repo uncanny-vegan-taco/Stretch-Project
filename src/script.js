fc832af395ea99ffb56a60d640b8ff87a578260b
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-28 16:34:07 -0800

    Embedded videos

--- src/script.js
+++ src/script.js
@@ -9,5 +9,77 @@ neck.addEventListener("click", () => {
 let shoulders = document.querySelector("#shoulders");
 shoulders.addEventListener("click", () => {
   media.innerHTML =
-    '<iframe src="https://www.youtube.com/embed/JUP_YdYyfQw?si=ysTeSZMCmnjBM0hT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+    '<iframe src="https://www.youtube.com/embed/SedzswEwpPw?si=iMxVeB-LqZeWMdTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let chest = document.querySelector("#chest");
+chest.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/LNOucekw2co?si=Fdrui11lNSxuK_Ul" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let core = document.querySelector("#core");
+core.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/_Mx24iENIEY?si=hpbhLkgJckD6yjT7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let back = document.querySelector("#back");
+back.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/phuS5VLQy8c?si=eXhG6Vw69G1RBeXW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let hips = document.querySelector("#hips");
+hips.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/Ho9em79_0qg?si=ZkvDkoAzDdDTd1ii" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let glutes = document.querySelector("#glutes");
+glutes.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/oMOgnSMcfM0?si=wiZZ53OWBGhweB_b" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let quads = document.querySelector("#quads");
+quads.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/dpsB_WtHlV0?si=UcZkaRPoYBZ3jfJo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let hamstrings = document.querySelector("#hamstrings");
+hamstrings.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/-Mirm7LKvKk?si=qF5Xpn34AUqA5iof" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let itBand = document.querySelector("#it-band");
+itBand.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/yXSAmpXcTE0?si=_HCJ5l6ixp6Yi1fp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let shins = document.querySelector("#shins");
+shins.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/0hTllAb4XGg?si=9gccHH9wZ-t4pvJU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let calves = document.querySelector("#calves");
+calves.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/zPzSkLHp9ws?si=OF7WoZiIYl_FItgz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let feet = document.querySelector("#feet");
+feet.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/ea4MF7IqILA?si=_awnnP0ohoKWohbB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+});
+
+let fullbody = document.querySelector("#fullbody");
+fullbody.addEventListener("click", () => {
+  media.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/GLy2rYHwUqY?si=LjcAGayVl5uVfROC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 });
